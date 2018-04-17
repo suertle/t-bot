d1a87342c1a2116bec682ddf10fb5b699f5f31cc
Teeraphat <teeraphat.btt@gmail.com>
2018-04-17 16:22:31 +0700

    detect new listing

--- src/change-detector.js
+++ src/change-detector.js
@@ -2,37 +2,55 @@ const
   request  = require('request'),
   scrapeIt = require("scrape-it")
 
-let previousValue = 'Binance is Now Available in Turkish'
-let index = 0
+let previousValue = {
+  listing: 'Binance Adds ADA/BNB and ADA/USDT Trading Pairs',
+  news: 'Binance is Now Available in Turkish'
+}
 let domain = 'https://support.binance.com'
-let src = [
-  {
-    url: 'https://support.binance.com/hc/en-us/categories/115000056351-Announcements',
-    selector: {
-      news: 'body > main > div.container > div > div > div > section:nth-child(2) > ul > li:nth-child(1)',
+let src = {
+  url: 'https://support.binance.com/hc/en-us/categories/115000056351-Announcements',
+  selector: {
+    listing: {
+      text: 'body > main > div.container > div > div > div > section:nth-child(1) > ul > li:nth-child(1)',
+      link: 'body > main > div.container > div > div > div > section:nth-child(1) > ul > li:nth-child(1) > a'
+    },
+    news: {
+      text: 'body > main > div.container > div > div > div > section:nth-child(2) > ul > li:nth-child(1)',
       link: 'body > main > div.container > div > div > div > section:nth-child(2) > ul > li:nth-child(1) > a'
     }
-  },
-]
+  }
+}
 
 exports.checkValue = function() {
-  scrapeIt(src[index].url, {
-    news: src[index].selector.news,
-    link: {
-      selector: src[index].selector.link,
+  scrapeIt(src.url, {
+    listingText: src.selector.listing.text,
+    listingLink: {
+      selector: src.selector.listing.link,
+      attr: "href"
+    },
+    newsText: src.selector.news.text,
+    newsLink: {
+      selector: src.selector.news.link,
       attr: "href"
     }
   }).then(( {data, response} ) => {
-    const newValue = data.news
-    const link = data.link
-    if (newValue && previousValue !== newValue) {
-      sendMessage(newValue + '\n' + domain + link)
-      console.log(`${previousValue} -> ${newValue}`)
-      previousValue = newValue
+    const listingText = data.listingText
+    const listingLink = data.listingLink
+    const newsText = data.newsText
+    const newsLink = data.newsLink
+    // check listing
+    if (listingText && listingText !== previousValue.listing) {
+      sendMessage(listingText + '\n' + domain + listingLink)
+      console.log("listing change")
+      previousValue.listing = listingText
     }
-    else {
-      console.log(newValue)
+    // check news
+    if (newsText && newsText !== previousValue.news) {
+      sendMessage(newsText + '\n' + domain + newsLink)
+      console.log("news change")
+      previousValue.news = newsText
     }
+    console.log(listingText + ' / ' + newsText)
   }).catch(e => {
     console.error('ERROR => ', e)
   })
